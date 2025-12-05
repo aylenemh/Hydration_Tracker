8d1df518a2a9d1c818f9e8a9f9a135226df431df
aylenemhernandez <aylenemhernandez@gmail.com>
2025-12-05 12:59:54 -0500

    Base HTML and functions with code organization.

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
-from hydration_engine import hydration_engine
+from flask import Flask, render_template, request, jsonify
+from functions.hydration_engine import hydration_engine  
 
 app = Flask(__name__)
 
-@app.route("/calculate", methods=["POST"])
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    data = request.get_json()
+
+    weight = data.get("weight", 0)
+    sex = data.get("sex", "male")
+    duration = data.get("duration", 0)
+    heart_rate = data.get("heart_rate", 0)
+    temp = data.get("temp", 22)
+
+    results = hydration_engine(weight, sex, duration, heart_rate, temp)
+
+    return jsonify(results)
+
+
 if __name__ == "__main__":
-    app.run(debug=True,port=5001)
+    app.run(debug=True)
+
 
 
